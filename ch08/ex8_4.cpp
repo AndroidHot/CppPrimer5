e046cd0f0358859d347a50b6aacce0ba67b87c71
AndroidHot <zhiwei.dev@gmail.com>
2019-02-27 22:30:01 +0800

    OverView:Finish exercise 8.2.1
    Description:
    
    Other info:

---
+++ ch08/ex8_4.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using std::ifstream;
+using std::string;
+using std::vector;
+
+void func(const string &file_name, vector<string> &vec)
+{
+    ifstream input(file_name);
+    if (input)
+    {
+        string buf;
+        while (std::getline(input, buf))
+        {
+            vec.push_back(buf);
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<string> vec;
+    func("../res/book.txt", vec);
+    for (const auto &str : vec)
+    {
+        std::cout << str << std::endl;
+    }
+    return 0;
+}
---
+++ ch08/ex8_6.cpp
@@ -0,0 +1,38 @@
+#include <fstream>
+#include <iostream>
+
+#include "../ch07/ex7_26.h"
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::ifstream;
+
+int main(int argc, char **argv)
+{
+    ifstream input(argv[1]);
+
+    Sales_data total;
+    if (read(input, total))
+    {
+        Sales_data trans;
+        while (read(input, trans))
+        {
+            if (total.isbn() == trans.isbn())
+            {
+                total.combine(trans);
+            }
+            else
+            {
+                print(cout, total) << endl;
+                total = trans;
+            }
+        }
+        print(cout, total) << endl;
+    }
+    else
+    {
+        cerr << "No data?!" << endl;
+    }
+
+    return 0;
+}
