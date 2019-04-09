b707ea1c843c3822d90b8723f1c1874ab247663f
AndroidHot <zhiwei.dev@gmail.com>
2019-04-09 22:29:03 +0800

    OverView:Finish exercise 13.1.2
    Description:

---
+++ ch13/ex13_8.h
@@ -0,0 +1,29 @@
+#include <string>
+
+using std::string;
+
+class HasPtr
+{
+  public:
+    HasPtr(const string &s = string()) : ps(new string(s)), i(0){};
+    ~HasPtr(){};
+
+    HasPtr(const HasPtr &hp) : ps(new string(*hp.ps)), i(hp.i){};
+    HasPtr &operator=(const HasPtr &rhs_hp);
+
+  private:
+    string *ps;
+    int i;
+};
+
+HasPtr &HasPtr::operator=(const HasPtr &rhs_hp)
+{
+    if (this != &rhs_hp)
+    {
+        delete ps;
+        ps = new string(*rhs_hp.ps);
+        i = rhs_hp.i;
+    }
+
+    return *this;
+}
