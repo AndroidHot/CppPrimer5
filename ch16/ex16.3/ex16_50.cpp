e296028be6aa089275a53e12902f4908cb0f8d64
AndroidHot <zhiwei.dev@gmail.com>
2020-11-02 22:08:41 +0800

    Finish exercise 16.3

---
+++ ch16/ex16.3/ex16_50.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+template <typename T>
+void f(T)
+{
+    printf("1\n");
+}
+
+template <typename T>
+void f(const T *)
+{
+    printf("2\n");
+}
+
+template <typename T>
+void g(T)
+{
+    printf("3\n");
+}
+
+template <typename T>
+void g(T *)
+{
+    printf("4\n");
+}
+
+int main()
+{
+    int i = 42, *p = &i;
+    const int ci = 0, *p2 = &ci;
+
+    g(42);
+    g(p);
+    g(ci);
+    g(p2);
+
+    f(42);
+    f(p);
+    f(ci);
+    f(p2);
+}
